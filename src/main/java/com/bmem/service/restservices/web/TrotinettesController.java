5e5a054ae302eaa8cc9e1108443035e2bde76d22
xoriy <kerdoudtahar@gmail.com>
2018-12-05 23:42:42 +0100

    modification du micro service en vu de l'implémentation d'un service de configuration

---
+++ src/main/java/com/bmem/service/restservices/web/TrotinettesController.java
@@ -0,0 +1,36 @@
+package com.bmem.service.restservices.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class TrotinettesController {
+ 
+
+	 @Value("${xParam}")
+	 private int xParam;
+	 
+	 @Value("${yParam}")
+	 private int yParam;
+	  
+	 @Value("${xParam}")
+	 private String me;
+	 
+
+
+	@GetMapping("/myConfig")
+	public Map<String, Object> myConfig(){
+		Map<String, Object> param = new HashMap<>();
+		param.put("xParam", xParam);
+		param.put("yParam", yParam);
+		param.put("me", me);
+		param.put("threadName", Thread.currentThread().getName());
+		return param;
+	}
+}
